00e95b3c36da86861a125ba2a0d92dec7eee1ccb
navule <PavanKumar.Navule@cdk.com>
2021-11-24 13:57:25 +0530

    Chapter 7 Functions

--- Chapter07/02_understanding_funtion_features/03_function_return_multiple_values/02_ignore_function_return_value.v
+++
@@ -1,7 +0,0 @@
-fn greet_and_message_length(name string) (string, int) {
-	mut greeting := 'Hello, ' + name + '!'
-	return greeting, greeting.len
-}
-
-i, _ := greet_and_message_length('Navule')
-println(i)
